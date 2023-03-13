93b29f709a8bf4bd059aa2e710d7eadd87c0f9d8
pekanka <justshura11@gmail.com>
2023-03-13 04:40:07 +0300

    Added page data parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import lxml
+import re
 
 domen = "https://aster.kz"
 
@@ -28,11 +29,20 @@ def pars_page(html_code):
         "wheel": None
     }
     soup = bs(html_code, "lxml")
-    soup.find("span", string = "Год выпуска").find_next_sibling()
+    discr["age"] = int(soup.find("span", string = "Год выпуска").find_next_sibling().text)
+    discr["eng_v"] = float(re.findall(r"\S*", soup.find("span", string = "Двигатель").find_next_sibling().text)[0])
+    discr["eng_l"] = float(re.findall(r"[(]\d*", soup.find("span", string = "Двигатель").find_next_sibling().text)[0][1:])
+    discr["drive"] = re.findall(r"[(]\w*", soup.find("span", string = "Привод").find_next_sibling().text)[0][1:]
+    discr["mileage"] = float(re.findall(r"[0-9 ]*", soup.find("span", string = "Пробег, км").find_next_sibling().text)[0].replace(" ", ""))
+    discr["fuel_type"] = soup.find("span", string = "Тип топлива").find_next_sibling().text.lower()
+    discr["transmission"] = re.findall(r"[(]\w*", soup.find("span", string = "Коробка передач").find_next_sibling().text)[0][1:].lower()
+    discr["wheel"] = soup.find("span", string = "Руль").find_next_sibling().text.lower()
+    return(discr)
 
 
-#req = requests.get(url = domen + "/cars?page=1")
-with open("test.html", "r", encoding = "utf-8") as f:
-    code = f.read()
-    pars_page(code)
+req = requests.get(url = "https://aster.kz/catalog/236767")
+if req.status_code == 200:
+    res = pars_page(req.text)
+    for i in res.keys():
+        print(i + ": " + str(res[i]))
 
